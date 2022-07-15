f7105e8f630b0dad9738b891b306386e87b51522
JayPark7821 <pjhyun1990@gmail.com>
2022-07-15 17:59:59 +0900

    jpa 활용 2 api개발 기본

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,9 +1,11 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,6 +26,7 @@ public class Member {
     //사용자가 여러개의 주문을할 수 있다
     // 주인이아니다 거울이다
 
+//    @JsonIgnore
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 
